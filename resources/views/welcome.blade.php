029a0c73631b90a63003f58db7bd34c91d51961f
lucasmassing <202782@upf.br>
2025-05-20 20:16:50 -0300

    Atualizado welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <p>Seja bem vindo ao sistema</p>
 <ul>
-    <li><a href="{{ url('/types') }}">Cadastro de tipos</a></li>
-    <li><a href="{{ url('/products') }}">Cadastro de produtos</a></li>
+    <li><a href="{{ url('/types') }}">Manutenção de tipos</a></li>
+    <li><a href="{{ url('/products') }}">Manutenção de produtos</a></li>
 </ul>
